05ece8c3301d4255e7edc368708a7cef37217e7e
Sam Apter <sam@sapter.com>
2019-08-03 23:21:55 -0400

    lecture 250

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Burger from "../../Burger/Burger";
+import Button from "../../UI/Button/Button";
+import classes from "./CheckoutSummary.module.css";
+
+const checkoutSummary = props => {
+  return (
+    <div className={classes.CheckoutSummary}>
+      <h1>We hope it tastes well!</h1>
+      <div className={classes.Burger}>
+        <Burger ingredients={props.ingredients} />
+      </div>
+      <Button btnType="Danger" click={props.click}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" click={props.click}>
+        CONTINUE
+      </Button>
+    </div>
+  );
+};
+
+export default checkoutSummary;
---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import classes from "./Checkout.module.css";
+
+import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+
+class Checkout extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      cheese: 1,
+      bacon: 1,
+      meat: 1,
+    },
+  };
+
+  render() {
+    return (
+      <div className={classes.Checkout}>
+        <CheckoutSummary ingredients={this.state.ingredients} />
+      </div>
+    );
+  }
+}
+
+export default Checkout;
